4de1e9b608cc5611bcd8ca3a94aa4edd54491916
deyvisnvg <deyvisnvg@gmail.com>
2023-01-25 20:02:57 -0500

    connect repo

---
+++ src/pages/api/users.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server"
+
+export const config = {
+    runtime: "edge",
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (req: NextRequest) => {
+    return NextResponse.json({
+        ok: true,
+        data: [
+            {
+                id: 1,
+                name: "deyvis"
+
+            },
+            {
+                id: 2,
+                name: "daniela"
+
+            },
+        ]
+    });
+};
